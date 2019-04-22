2d94507809736faee6d31046a52b9cd3e42e2a2c
Xiaoguang Zhao <cooleaf@gmail.com>
2019-04-22 09:53:42 +0800

    merge ch1 and ch2

---
+++ src/ch2/fib/fib_test.go
@@ -0,0 +1,34 @@
+package fib
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFibList(t *testing.T) {
+	// var a int = 1
+	// var b int = 1
+	var (
+		a int = 1
+		b     = 1
+	)
+
+	fmt.Print(a, " ")
+	for i := 0; i < 5; i++ {
+		fmt.Print(" ", b)
+		tmp := a
+		a = b
+		b = tmp + a
+	}
+	fmt.Println()
+}
+
+func TestExchange(t *testing.T) {
+	a := 1
+	b := 2
+	// tmp := a
+	// a = b
+	// b = a
+	a, b = b, a
+	t.Log(a, b)
+}
